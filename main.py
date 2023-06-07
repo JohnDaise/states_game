3f1658f28bb6cc86fd50db33ab3ba1a748d87ba7
JohnDaise <jdaise@gmail.com>
2023-06-07 16:54:44 -0600

    pulled states data

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import turtle
 import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+state_data = pandas.read_csv("50_states.csv")
+
+guessed_state = state_data[state_data["state"] == answer_state.title()]
+print(guessed_state)
